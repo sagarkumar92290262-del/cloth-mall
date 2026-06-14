ac7aae13206eeea0c8f407956884bda2f8996469
sagarkumar92290262-del <sagarkumar92290262@gmail.com>
2026-06-14 10:34:06 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,42 @@
+alert(JSON.stringify(JSON.parse(localStorage.getItem("products"))));
+let products = JSON.parse(localStorage.getItem("products") || "[]");
+
+let container = document.getElementById("product-container");
+
+let urlParams = new URLSearchParams(window.location.search);
+
+let selectedCategory = urlParams.get("cat");
+
+if(selectedCategory){
+
+products = products.filter(product =>
+product.category === selectedCategory
+);
+
+}
+
+if(container){
+
+products.forEach(product => {
+
+let card = document.createElement("div");
+
+card.className = "card";
+ card.innerHTML = `
+<img src="${product.image}" alt="${product.name}">
+<h3>${product.name}</h3>
+<p>₹${product.price}</p>
+<button onclick="alert('${product.name}')">
+Add To Cart
+</button>
+`;
+let btn = card.querySelector(".cart-btn");
+
+btn.addEventListener("click", function(){
+addToCart(product.name, product.price, product.image);
+});
+container.appendChild(card);
+
+});
+
+} 
